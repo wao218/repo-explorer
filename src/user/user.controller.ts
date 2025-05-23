e8a0b926b2333546296e054b4152f29da9c02bc9
wao218 <wesleyosborne1@gmail.com>
2025-05-22 20:03:26 -0400

    wip: added favorite routes

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,6 +1,21 @@
-import { Controller, UseGuards } from '@nestjs/common';
+import { Controller, Delete, Get, Post, UseGuards } from '@nestjs/common';
 import { JwtAuthGuard } from '../auth/guard/jwt-auth.guard';
+import { UserService } from './user.service';
 
 @UseGuards(JwtAuthGuard)
 @Controller('user')
-export class UserController {}
+export class UserController {
+  constructor(private userService: UserService) {}
+
+  // Get user's saved Repos
+  @Get('favorites')
+  async getFavoriteRepos() {}
+
+  // save a new repo
+  @Post('favorites')
+  async addToFavoriteRepos() {}
+
+  // remove a saved repo
+  @Delete('favorites/:id')
+  async deleteFavoriteRepoById() {}
+}
